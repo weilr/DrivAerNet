71669abb2d5f5f8a0427ef83823433f9dc7da204
lrwei <806871005@qq.com>
2025-03-31 21:51:31 +0800

    add cache

--- DeepSurrogates/DrivAerNetDataset.py
+++ DeepSurrogates/DrivAerNetDataset.py
@@ -173,7 +173,7 @@ class DrivAerNetDataset(Dataset):
         return vertices
 
     def _load_point_cloud(self, design_id: str) -> Optional[torch.Tensor]:
-        load_path = os.path.join(self.root_dir, 'cache', f"{design_id}_{self.num_points}.pt")
+        load_path = os.path.join(self.root_dir, f"{design_id}.pt")
         if os.path.exists(load_path) and os.path.getsize(load_path) > 0:
             try:
                 return torch.load(load_path)
@@ -222,19 +222,19 @@ class DrivAerNetDataset(Dataset):
             design_id = row['Design']
             cd_value = row[self.target]
 
-            vertices = self._load_point_cloud(design_id)
-            if vertices is None:
-                geometry_path = os.path.join(self.root_dir, f"{design_id}.stl")
-                if os.path.exists(geometry_path) and os.path.getsize(geometry_path) > 0:
-                    logging.warning(
-                        f"[Dataset] Skipping design {design_id} because point cloud is not found or corrupted.")
+            if self.pointcloud_exist:
+                vertices = self._load_point_cloud(design_id)
+
+                if vertices is None:
+                    logging.warning(f"[Dataset] Skipping design {design_id} because point cloud is not found or corrupted.")
                     idx = (idx + 1) % len(self.data_frame)
                     continue
+            else:
+                geometry_path = os.path.join(self.root_dir, f"{design_id}.stl")
                 try:
                     mesh = trimesh.load(geometry_path, force='mesh')
                     vertices = torch.tensor(mesh.vertices, dtype=torch.float32)
                     vertices = self._sample_or_pad_vertices(vertices, self.num_points)
-                    self._save_point_cloud(design_id, vertices)
 
                 except Exception as e:
                     logging.error(f"[Dataset] Failed to load STL file: {geometry_path}. Error: {e}")
