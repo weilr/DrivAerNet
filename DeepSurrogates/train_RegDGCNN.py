c5cacd42b229618b7924bbc15eb5d93f7afc71ab
lrwei <806871005@qq.com>
2025-04-02 23:34:49 +0800

    1.add adamw optimizer, 2.add easystopping, 3.change ReduceLROnPlateau patience=3

--- DeepSurrogates/train_RegDGCNN.py
+++ DeepSurrogates/train_RegDGCNN.py
@@ -217,7 +217,7 @@ def train_and_evaluate(model: torch.nn.Module, train_dataloader: DataLoader, val
     #     else optim.SGD(model.parameters(), lr=config['lr'], momentum=0.9, weight_decay=1e-4)
 
     # Initialize the learning rate scheduler (ReduceLROnPlateau) to reduce the learning rate based on validation loss
-    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=3, factor=0.1, verbose=True,eps=float('1e-15'))
+    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=10, factor=0.1, verbose=True,eps=float('1e-15'))
     early_stopping = EarlyStopping(patience=20, verbose=True)
     best_model_path = os.path.join(proj_path, 'models', f'{config["exp_name"]}_best_model.pth')
 
